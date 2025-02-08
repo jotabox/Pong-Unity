5f2e023ebd4534762a5a9006486e2f41b1b5d4c3
Jota <jotapaulo.box@gmail.com>
2025-02-07 22:17:53 -0300

    5º commit UI e audio gol
    add o áudio quando marca ponto , confirmação sonora da pontuação,
    
    e add a confirmação visual através da UI Text , quando é marcado pontos de ambos os lados

--- Pong/Assets/Scripts/Outros/GameManager.cs
+++ Pong/Assets/Scripts/Outros/GameManager.cs
@@ -1,18 +1,48 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]private int pointPlayer1;
-    [SerializeField]private int pointPlayer2;
+    [SerializeField] private int pointPlayer1 = 0 ;
+    [SerializeField] private int pointPlayer2 = 0;
 
+    [SerializeField] private Text _points;
+    private AudioSource _audio;
 
+    
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+        ChangeText();
+        if ( _points != null)
+        {
+            Debug.Log("o componente text , não foi atribuido no GameManager");
+        }
+    }
     public void AddPointPlayer1()
     {
         pointPlayer1++;
+        ChangeText();
+        PlayAudioGoal();
     }
 
     public void AddPointPlayer2()
     {
         pointPlayer2++;
+        ChangeText();
+        PlayAudioGoal();
+    }
+
+    private void ChangeText()
+    {
+        _points.text = $"{pointPlayer1} X {pointPlayer2}";
+
+    }
+
+    private void PlayAudioGoal()
+    {
+        _audio.Play();
     }
 }
